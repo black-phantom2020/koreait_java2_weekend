ea67e75a6e315b9a842a05749ad7ebb8131797f5
black-phantom2020 <chrimuelse@naver.com>
2021-03-06 16:18:49 +0900

    21.03.06. 클래스 예제 진행중

---
+++ com.kita.second/src/com/kita/second/level1/practice/Adventurer.java
@@ -0,0 +1,24 @@
+package com.kita.second.level1.practice;
+
+public class Adventurer {
+	// 필드
+	int strength;
+	int dexterity;
+	int intellegence;
+	int luck;
+	int hp;
+	// 생성자
+	// 기본스텟 전부 10으로 맞추기
+	
+	
+	// 메소드
+	attack() {}
+	jump() {}
+}
+
+
+
+
+
+
+
